2bd6875e2f6d910807c301a47ca005ce36e428f4
Jorge Christino <jorgecsantofer@gmail.com>
2024-11-10 14:26:40 -0300

    Configure Redux

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const middleware = (getDefaultMiddleware) => getDefaultMiddleware();
+const reducer = combineReducers({});
+
+const store = configureStore({
+  reducer,
+  middleware,
+});
+
+export default store;
